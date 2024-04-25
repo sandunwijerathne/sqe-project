97b2a2cb9e7fadf1d89a69b331dad15301199642
sandunwijerathne <sandunwijerathne51@gmail.com>
2024-04-25 19:22:14 +0530

    add important files to project

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost","root","","jobseeker");
+if(!$con){
+    die("Connection Failed");
+}
+
+?>
--- pages/cvtemplate.php
+++ pages/cvtemplate.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION["email"]) || $_SESSION["email"] !== true) {
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
-    Material Dashboard 2 by Creative Tim
+    JobSeeker Dashboard by JobSeeker Team
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
